78b785af8f010f7cb7233eb5b1929e6c6ce29aa7
Vitaly Komkov <Vitaliykomkov@gmail.com>
2021-06-12 21:20:27 +0300

    add link editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,16 +1,25 @@
 import { Editable, Slate, withReact } from "slate-react";
-
 import { createEditor } from "slate";
-import { useMemo } from "react";
+import { useCallback, useMemo, useRef } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import { isLinkNodeAtSelection } from "../utils/EditorUtils";
+
 import useEditorConfig from '../hooks/useEditorConfig';
 import useSelection from '../hooks/useSelection';
-import { useCallback } from "react";
+
 import Toolbar from "./Toolbar";
+import LinkEditor from "./LinkEditor"
+
 
 export default function Editor({ document, onChange }) {
+  const editorRef = useRef(null)
   const editor = useMemo(() => withReact(createEditor()), []);
   const { renderElement, renderLeaf, onKeyDown } = useEditorConfig(editor);
-  const [selection, setSelection] = useSelection(editor);
+  const [previousSelection, selection, setSelection] = useSelection(editor);
   const onChangeHandler = useCallback(
     (document) => {
       onChange(document);
@@ -19,10 +28,44 @@ export default function Editor({ document, onChange }) {
     [editor.selection, onChange, setSelection]
   );
 
+  let selectionForLink = null;
+  if (isLinkNodeAtSelection(editor, selection)) {
+    selectionForLink = selection;
+  } else if (selection == null && isLinkNodeAtSelection(editor, previousSelection)) {
+    selectionForLink = previousSelection;
+  }
+
   return (
     <Slate editor={editor} value={document} onChange={onChangeHandler}>
-      <Toolbar selection={selection} />
-      <Editable renderElement={renderElement} renderLeaf={renderLeaf} onKeyDown={onKeyDown}/>
+      <Container className={"editor-container"}>
+        <Row>
+          <Col>
+            <Toolbar selection={selection} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="editor" ref={editorRef}>
+              {
+                selectionForLink != null ? 
+                  <LinkEditor 
+                    selectionForLink={selectionForLink}
+                    editorOffsets={
+                      editorRef.current != null ? 
+                        {
+                          x: editorRef.current.getBoundingClientRect().x,
+                          y: editorRef.current.getBoundingClientRect().y,
+                        } :
+                        null
+                      }
+                  /> :
+                  null
+              }
+              <Editable renderElement={renderElement} renderLeaf={renderLeaf} onKeyDown={onKeyDown}/>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </Slate>
   );
 }
---
+++ src/components/LinkEditor.js
@@ -0,0 +1,80 @@
+import { useRef, useEffect, useState, useCallback } from "react";
+import { Editor, Transforms } from "slate";
+import { ReactEditor, useSlateStatic } from "slate-react";
+
+import isUrl from "is-url";
+
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+
+export default function LinkEditor({ editorOffsets, selectionForLink }) {
+  const linkEditorRef = useRef(null);
+  const editor = useSlateStatic();
+
+  const [linkNode, path] = Editor.above(editor, {
+    at: selectionForLink,
+    match: (n) => n.type === "link",
+  });
+
+  const [linkURL, setLinkURL] = useState(linkNode.url);
+
+  useEffect(() => {
+    setLinkURL(linkNode.url);
+  }, [linkNode]);
+
+  const onLinkURLChange = useCallback(
+    (event) => setLinkURL(event.target.value),
+    [setLinkURL]
+  );
+
+  const onApply = useCallback(
+    (event) => {
+      Transforms.setNodes(editor, { url: linkURL }, { at: path });
+    },
+    [editor, linkURL, path]
+  );
+
+  useEffect(() => {
+    const linkEditorEl = linkEditorRef.current;
+    if(linkEditorEl == null){
+      return;
+    }
+    
+    const linkDOMNode = ReactEditor.toDOMNode(editor, linkNode);
+    const {
+      x: nodeX,
+      height: nodeHeight,
+      y: nodeY
+    } = linkDOMNode.getBoundingClientRect();
+
+    linkEditorEl.style.display = "block";
+    linkEditorEl.style.top = `${nodeY + nodeHeight - editorOffsets.y}px`;
+    linkEditorEl.style.left = `${nodeX - editorOffsets.x}px`;
+  }, [editor, editorOffsets.x, editorOffsets.y, linkNode]);
+
+  if(editorOffsets == null) {
+    return null;
+  }
+  
+  return (
+    <Card className={"link-editor"} ref={linkEditorRef}>
+      <Card.Body>
+        <Form.Control
+          size="sm"
+          type="text"
+          value={linkURL}
+          onChange={onLinkURLChange}
+        />
+        <Button
+          className={"link-editor-btn"}
+          size="sm"
+          variant="primary"
+          disabled={!isUrl(linkURL)}
+          onClick={onApply}
+        />
+      </Card.Body>
+    </Card>
+  );
+}
